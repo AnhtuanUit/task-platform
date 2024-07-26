fba12eceb9c776141f251fe625f6e02df126eb7a
TuanDo <anhtuan7194@gmail.com>
2024-07-26 22:40:43 +0700

    Setup django channel for send message to user

--- realtime/consumers.py
+++ realtime/consumers.py
@@ -8,24 +8,49 @@ class RealtimeConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"realtime_{self.room_name}"
+        self.user = self.scope["user"]
+        self.user_group_name = f"user_{self.user.id}"
 
-        # Join room group
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        if not self.user.is_authenticated:
+            await self.close()
+        else:
+            # Join user group
+            await self.channel_layer.group_add(self.user_group_name, self.channel_name)
 
-        await self.accept()
+            # Join room group
+            await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+            await self.accept()
 
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+        # Leave user group
+        await self.channel_layer.group_discard(self.user_group_name, self.channel_name)
 
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
+        target = text_data_json.get("target", "room")
+
+        if target == "user":
+            await self.send_message_to_user(message)
+        else:
+            await self.send_message_to_room(message)
+
+    async def send_message_to_room(self, message):
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name, {"type": "realtime.message", "message": message}
+        )
+
+    async def send_message_to_user(self, message):
+        # Send message to user group
+        await self.channel_layer.group_send(
+            self.user_group_name, {"type": "realtime.message", "message": message}
         )
 
     # Receive message from room group
