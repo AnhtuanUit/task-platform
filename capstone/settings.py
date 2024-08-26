b913e9b9f8fa4a2a8c053075a1a32789ba2b40ea
AnhtuanUit <AnhtuanUit@users.noreply.github.com>
2024-08-26 18:00:17 +0700

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -141,11 +141,17 @@ LOGIN_URL = "login"
 
 ASGI_APPLICATION = "capstone.asgi.application"
 
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [("127.0.0.1", 6379)],
+#         },
+#     },
+# }
+
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
     },
 }
